dd5ffe73207a07d1439a03b5116eec11be7c3916
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-01 12:35:26 +0530

    add methods to initialize,cleanup and publish

--- src/ziggurat/retry_interface.clj
+++ src/ziggurat/retry_interface.clj
@@ -1,1 +1,8 @@
-(ns ziggurat.retry-interface)
+(ns ziggurat.retry-interface
+  (:import (ziggurat.mapper MessagePayload)))
+
+(defprotocol RetryProtocol
+  (initialize [impl rabbitmq-config])
+  (cleanup [impl])
+  (publish-to-queue [impl queue-name queue-type message-payload])
+  (publish-to-channel-queue [impl channel-name queue-name queue-type message-payload]))
