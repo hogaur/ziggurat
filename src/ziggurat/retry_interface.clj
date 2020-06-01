0c193a955a95c09733d025f231621b4c17b75668
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-01 15:06:33 +0530

    add a doc for the protocol

--- src/ziggurat/retry_interface.clj
+++ src/ziggurat/retry_interface.clj
@@ -2,6 +2,7 @@
   (:import (ziggurat.mapper MessagePayload)))
 
 (defprotocol RetryProtocol
+  "A Protocol that defines the interface for retries in Ziggurat using message queue implementation."
   (initialize [impl rabbitmq-config])
   (cleanup [impl])
   (retry [impl queue-name queue-type message-payload])
