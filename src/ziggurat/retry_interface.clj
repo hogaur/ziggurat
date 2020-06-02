553a2d4d75c5c9002109599844afcb8918939b00
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-02 09:35:02 +0530

    fix multi-arity method definition

--- src/ziggurat/retry_interface.clj
+++ src/ziggurat/retry_interface.clj
@@ -1,10 +1,10 @@
-(ns ziggurat.retry-interface
-  (:import (ziggurat.mapper MessagePayload)))
+(ns ziggurat.retry-interface)
 
 (defprotocol RetryProtocol
   "A Protocol that defines the interface for retries in Ziggurat using message queue implementation."
   (initialize [impl rabbitmq-config])
   (cleanup [impl])
-  (retry [impl queue-name queue-type message-payload])
-  (retry [impl channel-name queue-name queue-type message-payload])
+  (retry
+    [impl message-payload queue-name queue-type]
+    [impl message-payload queue-name queue-type channel-name])
   (start-consumers [impl stream-routes]))
