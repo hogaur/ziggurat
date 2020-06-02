792635187412995e4e8e64dd97491da83971f02f
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-02 12:16:43 +0530

    add retries and rabbitmq retry wrapper

--- src/ziggurat/rabbitmq_retry_wrapper.clj
+++ src/ziggurat/rabbitmq_retry_wrapper.clj
@@ -1,6 +1,16 @@
 (ns ziggurat.rabbitmq-retry-wrapper
+  (:require [ziggurat.messaging.connection :as conn])
   (:import (ziggurat.retry_interface RetryProtocol)))
 
+(def rabbitmq-connection (atom nil))
+
+(defn start-rabbitmq-connection []
+  (reset! rabbitmq-connection (conn/start-connection)))
+
+(defn stop-rabbitmq-connection []
+  (conn/stop-connection rabbitmq-connection))
 
 (deftype RabbitMQRetry []
-  RetryProtocol)
+  RetryProtocol
+  (initialize [this] (start-rabbitmq-connection))
+  (cleanup [this]) (start-rabbitmq-connection))
---
+++ src/ziggurat/retries.clj
@@ -0,0 +1,15 @@
+(ns ziggurat.retries
+  (:require [mount.core :refer [defstate]]
+            [clojure.tools.logging :as log]
+            [ziggurat.retry-interface :as retry-interface]))
+
+(def retry-impl (atom nil))
+(defn initialize-retry-library [])
+
+(defstate retry-backend
+  :start (do (log/info "Initializing retries")
+             (initialize-retry-library)
+             (retry-interface/initialize @retry-impl))
+  :stop (do (log/info "Stopping retries")
+            (retry-interface/cleanup @retry-impl)))
+
