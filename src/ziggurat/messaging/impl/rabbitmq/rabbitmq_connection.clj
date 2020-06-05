3bdfbc5c4ff01754fb8aac5a268eb345083b3d75
shubhang.balkundi <2932-shubhang.balkundi@users.noreply.source.golabs.io>
2020-06-05 15:19:46 +0530

    extract rabbitmq connection

--- src/ziggurat/messaging/impl/rabbitmq/rabbitmq_connection.clj
+++ src/ziggurat/messaging/impl/rabbitmq/rabbitmq_connection.clj
@@ -1,1 +1,20 @@
-(ns ziggurat.messaging.impl.rabbitmq.rabbitmq-connection)
+(ns ziggurat.messaging.impl.rabbitmq.rabbitmq-connection
+  (:require [ziggurat.messaging.impl.rabbitmq.connection :as connection])
+  (:import (com.rabbitmq.client AMQP$Connection)
+           (io.opentracing.contrib.rabbitmq TracingConnection)))
+
+(def ^{:private true} connection (atom nil))
+
+(defn start-connection []
+  (if (or (instance? AMQP$Connection @connection)
+          (instance? TracingConnection @connection))
+    @connection
+    (do (reset! connection (connection/start-connection))
+        @connection)))
+
+(defn stop-connection []
+  (do
+    (connection/stop-connection connection)
+    (reset! connection nil)))
+
+(defn get-conn [] @connection)
